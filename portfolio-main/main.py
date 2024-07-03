4f96e58729ae61aefeb492b7bcc45bea65bc1fb5
wqkereee <166634536+wqkereee@users.noreply.github.com>
2024-07-03 21:30:15 +0500

    Add files via upload

---
+++ portfolio-main/main.py
@@ -0,0 +1,23 @@
+#Импорт
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+#Запуск страницы с контентом
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Динамичные скиллы
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_html = request.form.get('button_html')
+    return render_template('index.html', button_html=button_html, button_python=button_python)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
